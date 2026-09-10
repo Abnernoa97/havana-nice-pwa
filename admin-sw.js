b37022a09b992f2c94b4f1311bcf3d067afb6d74
Abnernoa97 <soloaplicaciones97@gmail.com>
2026-09-09 23:50:25 -0600

    feat: add admin PWA service worker

---
+++ admin-sw.js
@@ -0,0 +1,18 @@
+const CACHE='hn-admin-v1';
+const ASSETS=[
+  './admin.html',
+  './admin-manifest.json',
+  './admin-ui-v1.js',
+  './notifications-admin-fix.js',
+  './calendar-v1.js',
+  './havana-nice-icon-192.png',
+  './havana-nice-icon-512.png'
+];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  const url=new URL(e.request.url);
+  if(url.origin!==location.origin)return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+});
